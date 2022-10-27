267d2cdb1d8987a20ba0fad40356f1271f22394f
SMN947 <53320639+SMN947@users.noreply.github.com>
2022-10-27 17:17:43 -0500

    1 file modified

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import WindowModal from 'react-window-modal';
 
 import './style.css';
 
-const startPos = { x: 5, y: 5 };
-const size = {x: window.innerWidth - 10, y: window.innerHeight - 10};
+const startPos = { x: 5, y: 40 };
+const size = { x: window.innerWidth - 10, y: window.innerHeight - 80 };
 
 class App extends Component {
   render() {
     return (
       <div>
-      <WindowModal title="Test" pos={startPos} size={size}>
-        Hello from within the modal!
-      </WindowModal>
-        <WindowModal title="Test2" pos={startPos} size={size}>
-          Hello from within the modal!
+        <div>
+          <button>Abrir un caso por numero</button>
+        </div>
+        <WindowModal title="Reporte uno" pos={startPos} size={size}>
+          El PBI por aca
+        </WindowModal>
+        <WindowModal title="Reporte dos" pos={startPos} size={size}>
+          El PBI por aca
         </WindowModal>
       </div>
     );
