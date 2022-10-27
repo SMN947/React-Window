435fb17bce16f3f1240d740eb94e263b64a6f613
SMN947 <53320639+SMN947@users.noreply.github.com>
2022-10-27 17:05:36 -0500

    2 files modified, 1 deleted

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import WindowModal from 'react-window-modal';
 
-import App from './App';
+import './style.css';
+
+const startPos = { x: 5, y: 5 };
+const size = {x: window.innerWidth - 10, y: window.innerHeight - 10};
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <WindowModal title="Test" pos={startPos} size={size}>
+        Hello from within the modal!
+      </WindowModal>
+        <WindowModal title="Test2" pos={startPos} size={size}>
+          Hello from within the modal!
+        </WindowModal>
+      </div>
+    );
+  }
+}
 
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(<App />, rootElement);
